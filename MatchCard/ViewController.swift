254900418164c5372d248e490b15d7a926b948e6
edgardo.agno@gmail.com <edgardoagno@EDGARDOs-MacBook-Air.local>
2015-05-27 18:41:36 +0100

    Initial Commit

---
+++ MatchCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MatchCard
+//
+//  Created by EDGARDO AGNO on 27/05/2015.
+//  Copyright (c) 2015 EDGARDO AGNO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
