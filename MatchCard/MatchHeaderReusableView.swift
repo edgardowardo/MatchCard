ffd4f0d73fb31c106fc378cce2311559fccf9ec6
edgardo.agno@gmail.com <edgardoagno@EDGARDOs-MacBook-Air-2.local>
2015-06-24 15:07:13 +0100

    Just before Auto Layout
    ….

--- MatchCard/MatchHeaderReusableView.swift
+++ MatchCard/MatchHeaderReusableView.swift
@@ -14,11 +14,12 @@ class MatchHeaderReusableView : UICollectionReusableView {
     static let constantKind = "UICollectionElementKindHeader"
     static let constantReuseIdentifier = "MatchHeaderReusableView"
     static let constantCellWidth : CGFloat = UIScreen.mainScreen().bounds.size.width
-    static let constantCellHeight : CGFloat = 80
+    static let constantCellHeight : CGFloat = 86
     static let constantDefaultSize = CGSizeMake(constantCellWidth, constantCellHeight)
     @IBOutlet weak var leagueName: UILabel!
-    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var div: UILabel!    
     @IBOutlet weak var division: UILabel!
+    @IBOutlet weak var location: UILabel!
     @IBOutlet weak var date: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
